02751acacfc1c79c3277919464ae467bb07a6fd6
kmgaikwad97 <kmgaikwad97@gmail.com>
2024-06-30 01:54:00 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 export { default as Navbar } from './Components/Navbar/Navbar';
 export { default as Home } from './Components/Home/Home';
 export { default as About } from './Components/About/About';
@@ -11,12 +9,5 @@ export { default as Products } from './Components/Products/Products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
