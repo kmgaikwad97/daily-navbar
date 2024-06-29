31b5696d4f22cae198e991d205cc1a4551e8d291
kmgaikwad97 <kmgaikwad97@gmail.com>
2024-06-29 16:45:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
-import Home from './Components/Home/Home';
 import Products from './Components/Products/Products';
-import Navbar from './Components/Navbar/Navbar';
+import { FaFacebookF, FaUser, FaTwitter, FaInstagram, FaShoppingCart } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
-function App() {
+const App = () => {
   return (
-    <div className="app">
-      <Router>
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <Navbar
+        logo="MyLogo"
+        links={[
+          { path: "/", name: "Home" },
+          { path: "/products", name: "Products" },
+          { path: "/about", name: "About" },
+          { path: "/contact", name: "Contact" },
+        ]}
+        socialIcons={[FaFacebookF, FaTwitter, FaInstagram]}
+        userIcons={[FaMagnifyingGlass, FaShoppingCart, FaUser]}
+      />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home bannerImage="./images/shop-pexel.jpg" bannerHeading="Welcome" bannerSubHeading="Find Amazing Products Below" />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
