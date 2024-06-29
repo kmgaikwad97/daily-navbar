92776cce78ec42358af04db2a6197c3e0b49006f
kmgaikwad97 <kmgaikwad97@gmail.com>
2024-06-29 16:15:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,3 @@
-// import './App.css';
-// import About from './Components/About/About';
-// import Contact from './Components/Contact/Contact';
-// import Home from './Components/Home/Home';
-// import Navbar from './Components/Navbar';
-// // import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-// function App() {
-
-//   // const appRouter = createBrowserRouter([
-//   //   {
-//   //       path:"/",
-//   //       element:<Home/>
-//   //   },
-//   //   {
-//   //     path:"/about",
-//   //     element:<About/>
-//   //   },
-//   //   {
-//   //     path:"/contact",
-//   //     element:<Contact/>
-//   //   },
-    
-  
-//   // ]);
-
-
-
-//   return (
-//     <>
-//       {/* <Navbar/>
-//       <RouterProvider router={appRouter} /> */}
-//     </>
-//   );
-// }
-
-
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -45,21 +5,24 @@ import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Home from './Components/Home/Home';
 import Products from './Components/Products/Products';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div className="app">
+      <Router>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
 export default App;
-
